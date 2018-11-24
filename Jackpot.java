f4ae87655983ed47ad650abf18238ee04a56e37d
malnemark <malnemark@yahoo.com>
2018-11-24 19:07:38 +0100

    Commit of Jackpot contract and corresponding test

---
+++ Jackpot.java
@@ -0,0 +1,193 @@
+package com.jelurida.ardor.contracts;
+
+import nxt.addons.*;
+import nxt.http.APICall;
+import nxt.http.callers.GetBalanceCall;
+import nxt.http.callers.GetBlockCall;
+import nxt.http.callers.GetBlockchainTransactionsCall;
+import nxt.http.callers.SendMoneyCall;
+import nxt.http.responses.BlockResponse;
+import nxt.http.responses.TransactionResponse;
+import org.json.simple.JSONObject;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static nxt.blockchain.ChildChain.IGNIS;
+
+
+/**
+ * The Jackpot contract is triggered every pre-defined number of blocks, checks if somebody won and
+ * sends the jackpot to every winner. If there are more than one winner, the jackpot is divided.
+ * This is a block based contract.
+ *
+ * Contract Parameters
+ * chain - chain to monitor
+ * frequency - how many blocks to wait between payments
+ * collectionRs - the account that issued the assets (assuming that all assets this account has issued belong to the collection.
+ */
+public class Jackpot extends AbstractContract {
+
+    /**
+     * This is triggered every block, executes payout every _frequency_th block.
+     * payout is to every account that has met the requirements.
+     * @param context contract context
+     */
+    @Override
+    public void processBlock(BlockContext context) {
+        // Read contract configuration
+        int chainId = getContractParams().getInt("chain", 2);
+        int frequency = getContractParams().getInt("frequency", 100);
+        //String contractRs = context.getConfig().getAccountRs();
+        String collectionRs = getContractParams().getString("collectionRs", "");         //if none specified, this will make the contract crash.
+
+        // Check the height if it shold perform payment distribution on this height
+        int height = context.getHeight();
+        int nextrun = (height / frequency) * frequency + frequency;
+        if (height % frequency != 0) {
+            context.logInfoMessage("CONTRACT: Jackpot: ignore block at height %d, next run at height %d", height, nextrun);
+            return;
+        }
+        context.logInfoMessage("CONTRACT: Jackpot: run contract at height %d", height);
+
+        JA collectionAssets = getCollectionAssets(collectionRs);
+
+        List<TransactionResponse> payments = getPaymentTransactions(context, chainId, Math.max(height - frequency, 2), context.getConfig().getAccount());
+        if (payments.size() == 0) {
+            context.logInfoMessage("No incoming payments between block %d and %d", Math.max(0, height - frequency + 1), height);
+            return;
+        }
+        context.logInfoMessage("Incoming payments between block %d and %d, now checking against collection reqs", Math.max(0, height - frequency + 1), height);
+
+        Set<String> Senders = getUniqueSenders(payments);
+        List<String> Winners = Senders.stream().filter(Sender-> confirmJackpotForSender(Sender,collectionAssets,payments)).collect(Collectors.toList());
+
+        long balance = 0;
+
+        if (Winners.size() > 0){
+            JO response = GetBalanceCall.create(2).account(context.getConfig().getAccountRs()).call();
+            balance = Long.parseLong((String) response.get("balanceNQT"));
+
+
+            long fee = IGNIS.ONE_COIN;
+            long price = (balance- (fee*Winners.size())) / Winners.size();
+
+            Winners.forEach(Winner -> {
+                SendMoneyCall sendMoneyCall = SendMoneyCall.create(chainId).recipient(Winner).amountNQT(price).feeNQT(fee);
+                context.createTransaction(sendMoneyCall);
+                context.logInfoMessage("Incoming assets between block %d and %d. Account %s won the jackpot", Math.max(0, height - frequency + 1), height,Winner);
+            });
+            // done
+        }
+        // no winners found!
+        context.logInfoMessage("No set of incoming assets between block %d and %d won the jackpot", Math.max(0, height - frequency + 1), height);
+    }
+
+    /**
+     * Load all incoming payments to the contract account between the current height and the previously checked height
+     * @param context contract context
+     * @param chainId chain to monitor fot payments
+     * @param height load transactions from this height until the current height
+     * @param contractAccount the contract account
+     * @return list of incoming payment transactions
+     */
+    private List<TransactionResponse> getPaymentTransactions(AbstractContractContext context, int chainId, int height, String contractAccount) {
+        // Get the block timestamp from which to load transactions and load the contract account transactions
+        BlockResponse block = GetBlockCall.create().height(height).getBlock();
+        GetBlockchainTransactionsCall getBlockchainTransactionsResponse = GetBlockchainTransactionsCall.create(chainId).
+                timestamp(block.getTimestamp()).
+                account(context.getConfig().getAccountRs()).
+                executedOnly(true).
+                type(2).subtype(1);
+        List<TransactionResponse> transactionList = getBlockchainTransactionsResponse.getTransactions();
+
+        // Filter the transactions by recipient, ignore transactions the contract sent to itself
+        List<TransactionResponse> transactionsFiltered = transactionList.stream().filter(t -> {
+            String recipient = t.getRecipient();
+            if (!recipient.equals(contractAccount)) {
+                return false;
+            }
+            //noinspection RedundantIfStatement
+            if (recipient.equals(contractAccount) && t.getSender().equals(contractAccount)) {
+                return false;
+            }
+            return true;
+        }).collect(Collectors.toList());
+
+        return transactionsFiltered;
+    }
+
+    private Set<String> getUniqueSenders(List<TransactionResponse> transactionList){
+        List<String> Senders = transactionList.stream().map(TransactionResponse::getSender).collect(Collectors.toList());
+        return new HashSet<String>(Senders);
+    }
+
+    private JA getCollectionAssets(String account){
+        APICall getCollectionAssets = new APICall.Builder("getAssetsByIssuer").
+                param("account", account).
+                build();
+        JO response = new JO(getCollectionAssets.invoke());
+        JA schachtel = new JA(response.get("assets")); // Need to unbox another array
+        return new JA(schachtel.getObject(0));
+    }
+
+    private boolean confirmJackpotForSender(String Sender, JA collectionAssets, List<TransactionResponse> transactionList){
+        ListIterator caItr = collectionAssets.listIterator();
+        while (caItr.hasNext()) {
+            JSONObject assetObj = (JSONObject) caItr.next();
+            JO asset = new JO(assetObj);
+            String assetId = asset.getString("asset");
+            Object[] matching = transactionList
+                    .stream()
+                    .filter(payment -> payment.getAttachmentJson().getString("asset").equals(assetId) & payment.getSender().equals(Sender))
+                    .toArray();
+            if (matching.length == 0){
+                // this asset not found, no jackpot for you..
+                return false;
+            }
+            if (matching.length >1){
+                // exceptional case, the guy send this one twice.
+                // TODO cater for this case
+                return false;
+            }
+        }
+        // we came this far, so must be good.
+        return true;
+    }
+
+    /**
+     * Check the contract status
+     * Returns jackpot information and current status.
+     * @param context contract contract
+     */
+    @Override
+    public void processRequest(RequestContext context) {
+        context.logInfoMessage("CONTRACT: Jackpot: received API request.");
+
+        int frequency = getContractParams().getInt("frequency", 10);
+        int confirmationTime = getContractParams().getInt("confirmationTime", 5);
+        String collectionRs = getContractParams().getString("collectionRs", "");
+
+        String jackount = context.getConfig().getAccountRs();
+
+        // call contract to request information
+        JO response = GetBalanceCall.create(2).account(jackount).call();
+        long balance = Long.parseLong((String) response.get("balanceNQT"));
+
+
+        JA assets = getCollectionAssets(collectionRs);
+        int numAssets = assets.size();
+
+        JO retresponse = new JO();
+        retresponse.put("jackpotBalanceNQT", balance);
+        retresponse.put("jackpotAccountRs", jackount);
+        retresponse.put("collectionAccountRs", collectionRs);
+        retresponse.put("collectionSize", numAssets);
+        retresponse.put("jackpotRunFrequency", frequency);
+        retresponse.put("jackpotConfirmationTime", confirmationTime);
+        context.setResponse(retresponse);
+    }
+}
---
+++ tests/JackpotTest.java
@@ -0,0 +1,290 @@
+package com.jelurida.ardor.contracts;
+
+import nxt.Nxt;
+import nxt.addons.JA;
+import nxt.addons.JO;
+import nxt.blockchain.Block;
+import nxt.blockchain.ChildTransaction;
+import nxt.blockchain.FxtTransaction;
+import nxt.http.APICall;
+import nxt.http.callers.GetBalanceCall;
+import nxt.messaging.PrunablePlainMessageAppendix;
+import nxt.util.Convert;
+import nxt.util.Logger;
+import org.json.simple.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+import static com.jelurida.ardor.contracts.TarascaTester.initCollection;
+import static com.jelurida.ardor.contracts.TarascaTester.sendAssets;
+import static java.lang.Math.abs;
+import static nxt.blockchain.ChildChain.IGNIS;
+
+
+public class JackpotTest extends AbstractContractTest {
+
+    @Test
+    public void JackpotApi(){
+        Logger.logDebugMessage("Test JackpotApi()");
+        JO jackParams = new JO();
+        int contractFrequency = 3;
+        int collectionSize = 4;
+        jackParams.put("frequency",contractFrequency);
+        jackParams.put("collectionRs",CHUCK.getRsAccount());
+
+        initCollection(CHUCK.getSecretPhrase(), collectionSize);
+
+        String jackName = ContractTestHelper.deployContract(Jackpot.class,jackParams,false);
+
+        generateBlock();
+
+        // call contract to request information
+        APICall apiCall = new APICall.Builder("triggerContractByRequest").
+                param("contractName", "Jackpot").
+                build();
+        JO contractResponse = new JO(apiCall.invoke());
+
+        Assert.assertEquals(contractResponse.getInt("collectionSize"),collectionSize);
+        Assert.assertEquals(contractResponse.getString("jackpotAccountRs"),ALICE.getRsAccount());
+        Assert.assertEquals(contractResponse.getString("collectionAccountRs"),CHUCK.getRsAccount());
+        Assert.assertEquals(contractResponse.getInt("jackpotRunFrequency"),contractFrequency);
+    }
+
+    @Test
+    public void rejectWinnerIncompleteTx(){
+        Logger.logDebugMessage("Test rejectWinnerIncompleteTx()");
+        JO jackParams = new JO();
+        int contractFrequency = 4;
+        int confirmationTime = 1;
+        int collectionSize = 3;
+        jackParams.put("frequency",contractFrequency);
+        jackParams.put("collectionRs",CHUCK.getRsAccount());
+        jackParams.put("confirmationTime",confirmationTime);
+        initCollection(CHUCK.getSecretPhrase(), collectionSize);
+
+        String jackName = ContractTestHelper.deployContract(Jackpot.class,jackParams,false);
+
+        generateBlock();
+
+        APICall apiCallFull = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",ALICE.getRsAccount()).
+                build();
+        JO responseFull = new JO(apiCallFull.invoke());
+        long balanceFull = Long.parseLong((String) responseFull.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        APICall apiCallBobBefore = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",BOB.getRsAccount()).
+                build();
+        JO responseBobBefore = new JO(apiCallBobBefore.invoke());
+        long balanceBobBefore = Long.parseLong((String) responseBobBefore.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+
+        JA collectionAssets = TarascaTester.getCollectionAssets(CHUCK.getRsAccount());
+        sendAssets(collectionAssets,3,CHUCK.getSecretPhrase(),BOB.getRsAccount(),"to Bob");
+
+        generateBlock();
+        generateBlock();
+
+
+        JA notAllAssets = new JA();
+        for (int i=0;i<collectionAssets.size()-1;i++){
+            notAllAssets.add(collectionAssets.get(i));
+        }
+
+        //send all assets to contract, expectation is JACKPOT!
+        sendAssets(notAllAssets,1,BOB.getSecretPhrase(),ALICE.getRsAccount(),"to Contract ALICE");
+
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+
+        APICall apiCallEmpty = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",ALICE.getRsAccount()).
+                build();
+        JO responseEmpty = new JO(apiCallEmpty.invoke());
+        long balanceEmpty = Long.parseLong((String) responseEmpty.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        APICall apiCallBobAfter = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",BOB.getRsAccount()).
+                build();
+        JO responseBobAfter = new JO(apiCallBobAfter.invoke());
+        long balanceBobAfter = Long.parseLong((String) responseBobAfter.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+
+        long diffBob = balanceBobAfter-balanceBobBefore;
+        long diffAlice = balanceEmpty-balanceFull;
+
+        // nothing shouldve happened
+        Assert.assertTrue( balanceEmpty==balanceFull);
+        Assert.assertTrue( balanceBobAfter==balanceBobBefore-notAllAssets.size()); //this assumes 1 Ignis fee!
+    }
+
+    @Test
+    public void acceptSingleWinner(){
+        Logger.logDebugMessage("Test jackPotAccept()");
+        JO jackParams = new JO();
+        int contractFrequency = 4;
+        int confirmationTime = 1;
+        int collectionSize = 3;
+        jackParams.put("frequency",contractFrequency);
+        jackParams.put("collectionRs",CHUCK.getRsAccount());
+        jackParams.put("confirmationTime",confirmationTime);
+        initCollection(CHUCK.getSecretPhrase(), collectionSize);
+
+        String jackName = ContractTestHelper.deployContract(Jackpot.class,jackParams,false);
+
+        generateBlock();
+
+        APICall apiCallFull = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",ALICE.getRsAccount()).
+                build();
+        JO responseFull = new JO(apiCallFull.invoke());
+        long balanceFull = Long.parseLong((String) responseFull.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        APICall apiCallBobBefore = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",BOB.getRsAccount()).
+                build();
+        JO responseBobBefore = new JO(apiCallBobBefore.invoke());
+        long balanceBobBefore = Long.parseLong((String) responseBobBefore.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+
+        JA collectionAssets = TarascaTester.getCollectionAssets(CHUCK.getRsAccount());
+        sendAssets(collectionAssets,3,CHUCK.getSecretPhrase(),BOB.getRsAccount(),"to Bob");
+
+        generateBlock();
+        generateBlock();
+
+        //send all assets to contract, expectation is JACKPOT!
+        sendAssets(collectionAssets,1,BOB.getSecretPhrase(),ALICE.getRsAccount(),"to Contract ALICE");
+
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+
+        APICall apiCallEmpty = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",ALICE.getRsAccount()).
+                build();
+        JO responseEmpty = new JO(apiCallEmpty.invoke());
+        long balanceEmpty = Long.parseLong((String) responseEmpty.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        APICall apiCallBobAfter = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",BOB.getRsAccount()).
+                build();
+        JO responseBobAfter = new JO(apiCallBobAfter.invoke());
+        long balanceBobAfter = Long.parseLong((String) responseBobAfter.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+
+        long diffBob = balanceBobAfter-balanceBobBefore;
+        long diffAlice = balanceEmpty-balanceFull;
+
+        Assert.assertTrue( balanceEmpty<10); // 10 ignis tolerance for fees..
+        Assert.assertTrue( abs(diffBob + diffAlice)<10); // 10 ignis tolerance for fees..
+    }
+
+    @Test
+    public void dividePriceByTwo(){
+        Logger.logDebugMessage("Test jackPotDividePrice()");
+        JO jackParams = new JO();
+        int contractFrequency = 4;
+        int confirmationTime = 1;
+        int collectionSize = 3;
+        jackParams.put("frequency",contractFrequency);
+        jackParams.put("collectionRs",CHUCK.getRsAccount());
+        jackParams.put("confirmationTime",confirmationTime);
+        initCollection(CHUCK.getSecretPhrase(), collectionSize);
+
+        String jackName = ContractTestHelper.deployContract(Jackpot.class,jackParams,false);
+
+        generateBlock();
+
+        APICall apiCallFull = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",ALICE.getRsAccount()).
+                build();
+        JO responseFull = new JO(apiCallFull.invoke());
+        long balanceFull = Long.parseLong((String) responseFull.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        APICall apiCallBobBefore = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",BOB.getRsAccount()).
+                build();
+        APICall apiCallDaveBefore = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",DAVE.getRsAccount()).
+                build();
+        JO responseBobBefore = new JO(apiCallBobBefore.invoke());
+        long balanceBobBefore = Long.parseLong((String) responseBobBefore.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        JO responseDaveBefore = new JO(apiCallDaveBefore.invoke());
+        long balanceDaveBefore = Long.parseLong((String) responseDaveBefore.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+
+        JA collectionAssets = TarascaTester.getCollectionAssets(CHUCK.getRsAccount());
+        sendAssets(collectionAssets,3,CHUCK.getSecretPhrase(),BOB.getRsAccount(),"to Bob");
+        sendAssets(collectionAssets,3,CHUCK.getSecretPhrase(),DAVE.getRsAccount(),"to Dave");
+
+        generateBlock();
+        generateBlock();
+
+        sendAssets(collectionAssets,1,BOB.getSecretPhrase(),ALICE.getRsAccount(),"from Bob to Contract ALICE");
+
+        generateBlock();
+
+        sendAssets(collectionAssets,1,DAVE.getSecretPhrase(),ALICE.getRsAccount(),"from Dave to Contract ALICE");
+
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+
+        APICall apiCallEmpty = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",ALICE.getRsAccount()).
+                build();
+        JO responseEmpty = new JO(apiCallEmpty.invoke());
+        long balanceEmpty = Long.parseLong((String) responseEmpty.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        APICall apiCallBobAfter = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",BOB.getRsAccount()).
+                build();
+        JO responseBobAfter = new JO(apiCallBobAfter.invoke());
+        long balanceBobAfter = Long.parseLong((String) responseBobAfter.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        APICall apiCallDaveAfter = new APICall.Builder("getBalance").
+                param("chain", 2).
+                param("account",DAVE.getRsAccount()).
+                build();
+        JO responseDaveAfter = new JO(apiCallDaveAfter.invoke());
+        long balanceDaveAfter = Long.parseLong((String) responseDaveAfter.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+
+        long diffBob = balanceBobAfter-balanceBobBefore;
+        long diffDave = balanceDaveAfter-balanceDaveBefore;
+        long diffAlice = balanceEmpty-balanceFull;
+
+        Assert.assertTrue( balanceEmpty<10); // 10 ignis tolerance for fees..
+        Assert.assertTrue( abs(diffBob + diffAlice/2)<10); // 10 ignis tolerance for fees..
+        Assert.assertTrue( abs(diffDave+ diffAlice/2)<10);
+    }
+
+
+    @Test
+    public void emptyPotRejectWinner(){
+        Assert.assertTrue(false);
+    }
+}
