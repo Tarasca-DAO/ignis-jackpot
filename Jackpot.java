11806f381d4c3e87739dce3e519a2ae7b12b2e12
Mal Nemark <malnemark@yahoo.com>
2021-03-13 23:28:11 +0100

    first untested version with messages and updated tests.

--- Jackpot.java
+++ Jackpot.java
@@ -9,11 +9,7 @@ import nxt.addons.JA;
 import nxt.addons.JO;
 import nxt.addons.RequestContext;
 import nxt.blockchain.ChildChain;
-import nxt.http.callers.GetAssetsByIssuerCall;
-import nxt.http.callers.GetBalanceCall;
-import nxt.http.callers.GetBlockCall;
-import nxt.http.callers.GetBlockchainTransactionsCall;
-import nxt.http.callers.SendMoneyCall;
+import nxt.http.callers.*;
 import nxt.http.responses.BlockResponse;
 import nxt.http.responses.TransactionResponse;
 import org.json.simple.JSONArray;
@@ -21,39 +17,76 @@ import org.json.simple.JSONArray;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static nxt.blockchain.ChildChain.IGNIS;
+
 public class Jackpot extends AbstractContract {
 
     public JO processBlock(BlockContext context) {
         int chainId = context.getParams(Params.class).chainId();
         int frequency = context.getParams(Params.class).frequency();
         String collectionRs = context.getParams(Params.class).collectionRs();
+
         int height = context.getHeight();
-        if (height % frequency != 0) {
+        int modulo = (height-1) % frequency; // need to subtract 1 to get transition right (when jackpot block is reached)
+        int lastJackpotHeight = height - modulo;
+        int nextJackpotHeight = lastJackpotHeight + frequency;
+
+        context.logInfoMessage("CONTRACT: Jackpot: run at height %d with params chainId %d, frequency: %d, collection: %s (lastJackpot: %d, nextJackpot: %d", height, chainId, frequency, collectionRs,lastJackpotHeight,nextJackpotHeight);
+        List<JO> collectionAssets = getCollectionAssets(collectionRs);
+        List<TransactionResponse> payments = getPaymentTransactions(chainId, lastJackpotHeight+1, context.getAccount());
+
+        if (payments.size() == 0) {
             JO returned = new JO();
-            int nextrun = height / frequency * frequency + frequency;
-            returned.put("message", "Jackpot: ignore block at height " + height + ", next run at height " + nextrun);
+            returned.put("message", "CONTRACT: Jackpot: No incoming payments between block " + lastJackpotHeight + " and " + height + ", exit.");
             return returned;
         } else {
-            context.logInfoMessage("CONTRACT: Jackpot: run contract at height %d with params chainId %d, frequency: %d, collection: %s", height, chainId, frequency, collectionRs);
-            List<JO> collectionAssets = getCollectionAssets(collectionRs);
-            List<TransactionResponse> payments = getPaymentTransactions(chainId, Math.max(height - frequency + 1, 2), context.getAccount());
-            if (payments.size() == 0) {
-                context.logInfoMessage("CONTRACT: Jackpot: No incoming payments between block %d and %d", Math.max(0, height - frequency + 1), height);
-                return new JO();
-            } else {
-                context.logInfoMessage("CONTRACT: Jackpot: Incoming payments between block %d and %d, now checking against collection reqs", Math.max(0, height - frequency + 1), height);
-                Set<String> senders = getUniqueSenders(payments);
-                context.logInfoMessage("CONTRACT: Jackpot: getUniqueSenders(): " + senders.toString());
-                Map<String, Integer> winners = new HashMap<String, Integer>();
-                for(String sender : senders){
-                    int winnerJackpots = confirmJackpotsForSender(context, sender, context.getAccount(), collectionAssets, payments);
-                    winners.put(sender,winnerJackpots);
+            context.logInfoMessage("CONTRACT: Jackpot: Incoming payments between block %d and %d, now checking against collection reqs", lastJackpotHeight, height);
+
+            Set<String> senders = getUniqueSenders(payments);
+            context.logInfoMessage("CONTRACT: Jackpot: getUniqueSenders(): " + senders.toString());
+
+            Map<String, Integer> winners = new HashMap<String, Integer>();
+            for(String sender : senders){
+                int winnerJackpots = confirmJackpotsForSender(context, sender, context.getAccount(), collectionAssets, payments);
+                winners.put(sender,winnerJackpots);
+            }
+            context.logInfoMessage("CONTRACT: Jackpot: confirmJackpotForSender() Winners: " + winners.toString());
+
+            // check jackpot block or not
+            if (height % frequency != 0) {
+                context.logInfoMessage("CONTRACT: Jackpot: No jackpot block height, checking if messages need to be sent out");
+                BlockResponse block = GetBlockCall.create().height(height).getBlock();
+                int timestampLastJackpot = block.getTimestamp();
+                for (Map.Entry<String, Integer> entry : winners.entrySet()) {
+                    String winner = entry.getKey();
+                    Integer numWins = entry.getValue();
+                    JO response = GetPrunableMessagesCall.create(2).account(context.getAccountRs()).otherAccount(winner).timestamp(timestampLastJackpot).call();
+                    JA msgs = response.getArray("prunableMessages");
+                    context.logInfoMessage("Winner " + winner + ": found " + msgs.size() + " msgs for " + numWins+ " confirmed participations.");
+                    //TODO think do we need to read the messages? maybe not for the first version
+                    if (numWins > msgs.size()) {
+                        // TODO: add checking for already sent messages...
+                        context.logInfoMessage("sending message");
+                        JO message = new JO();
+                        message.put("contract", "Jackpot");
+                        message.put("currentHeight",height);
+                        message.put("jackpotHeight",nextJackpotHeight);//simplified, only works for the first round.
+                        message.put("participationConfirmed", true);
+                        JO msgresponse = SendMessageCall.create(2).
+                                message(message.toJSONString()).
+                                messageIsPrunable(true).
+                                recipient(winner).
+                                call();
+                    }
                 }
-                context.logInfoMessage("CONTRACT: Jackpot: confirmJackpotForSender() Winners: " + winners.toString());
+                JO returned = new JO();
+                returned.put("message", "Jackpot: finish block at height " + height + ", next run at height " + nextJackpotHeight + ", no pay out, not a jackpot block height, exit.");
+                return returned;
+            } else {
                 long balance;
                 int winnersSize = 0;
                 for (Map.Entry mapElement : winners.entrySet()) {
-                    int value = (int)mapElement.getValue();
+                    int value = (int) mapElement.getValue();
                     winnersSize += value;
                 }
                 if (winnersSize > 0) {
@@ -61,10 +94,10 @@ public class Jackpot extends AbstractContract {
                     balance = response.getLong("balanceNQT");
                     long fee = ChildChain.IGNIS.ONE_COIN;
                     long price = (balance - fee * winnersSize) / winnersSize;
-                    winners.forEach((winner,jackpots) -> {
+                    winners.forEach((winner, jackpots) -> {
                         context.logInfoMessage("CONTRACT: Jackpot: Incoming assets between block %d and %d. Account %s won the jackpot", Math.max(0, height - frequency + 1), height, winner);
-                        SendMoneyCall sendMoneyCall = SendMoneyCall.create(chainId).recipient(winner).amountNQT(price*jackpots).feeNQT(fee);
-                        context.logInfoMessage("CONTRACT: Jackpot: Send Prize: %d Ignis to %s", price*jackpots, winner);
+                        SendMoneyCall sendMoneyCall = SendMoneyCall.create(chainId).recipient(winner).amountNQT(price * jackpots).feeNQT(fee);
+                        context.logInfoMessage("CONTRACT: Jackpot: Send Prize: %d Ignis to %s", price * jackpots, winner);
                         context.createTransaction(sendMoneyCall);
                     });
                     context.logInfoMessage("CONTRACT: Jackpot: finished, exiting.");
--- tests/JackpotTest.java
+++ tests/JackpotTest.java
@@ -1,11 +1,11 @@
 package com.jelurida.ardor.contracts;
 
+import nxt.addons.ContractRunner;
 import nxt.addons.JA;
 import nxt.addons.JO;
 import nxt.http.APICall;
-import nxt.http.callers.GetAssetsByIssuerCall;
-import nxt.http.callers.TriggerContractByRequestCall;
-import nxt.http.callers.GetBalanceCall;
+import nxt.http.callers.*;
+import nxt.http.responses.BlockResponse;
 import nxt.util.Logger;
 import org.junit.Assert;
 import org.junit.Test;
@@ -260,9 +260,199 @@ public class JackpotTest extends AbstractContractTest {
         Logger.logDebugMessage("TEST: dividePriceByTwo(): Done");
     }
 
-    /*    @Test
-     *   public void emptyPotRejectWinner(){
-     *       Assert.assertTrue(false);
-     *   }
-     */
+
+    // this test checks that a message is sent for a winner, and only once. -> the jackpot block should be included in the message
+    // assert that in the next cycle after a player has a complete participation, a message is sent
+    // assert that no message is sent for incomplete participation
+    // assert: correct recipient, message content
+    @Test
+    public void sendMessageForWinner(){
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Start");
+        JO jackParams = new JO();
+        int contractFrequency = 9;
+        int confirmationTime = 1;
+        int collectionSize = 3;
+        jackParams.put("frequency",contractFrequency);
+        jackParams.put("collectionRs",ALICE.getRsAccount());
+        jackParams.put("confirmationTime",confirmationTime);
+        initCollection(collectionSize);
+
+        String jackName = ContractTestHelper.deployContract(Jackpot.class,jackParams,false);
+
+        generateBlock();
+
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Prepare accounts");
+        JO response = GetAssetsByIssuerCall.create().account(ALICE.getRsAccount()).call();
+        JA collectionAssets = response.getArray("assets").getArray(0);
+        sendAssets(collectionAssets,3,ALICE.getSecretPhrase(),BOB.getRsAccount(),"to Bob");
+        sendAssets(collectionAssets,3,ALICE.getSecretPhrase(),DAVE.getRsAccount(),"to Bob");
+
+        Logger.logDebugMessage("TEST: Accounts");
+        Logger.logDebugMessage("TEST: Contract (Alice): "+ALICE.getRsAccount()+", numeric: "+ALICE.getAccount());
+        Logger.logDebugMessage("TEST: Player   (Bob  ): "+BOB.getRsAccount()+", numeric: "+BOB.getAccount());
+        Logger.logDebugMessage("TEST: Player   (Dave ): "+DAVE.getRsAccount()+", numeric: "+DAVE.getAccount());
+
+        generateBlock();
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Start playing");
+
+        /*JO responseFull = GetBalanceCall.create(IGNIS.getId()).account(ALICE.getRsAccount()).call();
+        long balanceFull = Long.parseLong((String) responseFull.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        JO responseBobBefore = GetBalanceCall.create(IGNIS.getId()).account(BOB.getRsAccount()).call();
+        long balanceBobBefore = Long.parseLong((String) responseBobBefore.get("balanceNQT"))/IGNIS.ONE_COIN;*/
+
+        JA notAllAssets = new JA();
+        for (int i=0;i<collectionAssets.size()-1;i++){
+            notAllAssets.add(collectionAssets.get(i));
+        }
+
+        //send not all assets to contract, expectation is jackpot will reject BOB!
+        JO participationResponse = GetBlockCall.create().call();
+        int participationHeight = participationResponse.getInt("height");
+
+        sendAssets(notAllAssets,1,BOB.getSecretPhrase(),ALICE.getRsAccount(),"Bob to Contract ALICE");
+        sendAssets(collectionAssets,1,DAVE.getSecretPhrase(),ALICE.getRsAccount(),"Dave to Contract ALICE");
+
+        //mock up:
+        // Compose the message
+        JO current = GetBlockCall.create().call();
+        int currentHeight = current.getInt("height");
+        JO message = new JO();
+        message.put("contract", "Jackpot");
+        message.put("currentHeight",currentHeight);
+        message.put("jackpotHeight",contractFrequency);//simplified, only works for the first round.
+        message.put("participationConfirmed", true);
+        JO msgresponse = SendMessageCall.create(IGNIS.getId()).
+                secretPhrase(ALICE.getSecretPhrase()).
+                message(message.toJSONString()).
+                messageIsPrunable(true).
+                recipient(DAVE.getRsAccount()).
+                feeNQT(IGNIS.ONE_COIN*(long)0.5).
+                call();
+
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Evaluate results");
+
+        BlockResponse block = GetBlockCall.create().height(3).getBlock(); //TODO this is sketchy
+        GetBlockchainTransactionsCall.
+                create(IGNIS.getId()).
+                timestamp(block.getTimestamp()).
+                account(ALICE.getRsAccount()).
+                executedOnly(true).
+                type(2).
+                subtype(1).
+                call();
+
+        // assert that in the next cycle after a player has a complete participation, a message is sent
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Asserting that a complete participation results in a message to recipient");
+        current = GetBlockCall.create().call();
+        currentHeight = current.getInt("height");
+        int lastJackpotHeight = currentHeight - (currentHeight % contractFrequency);
+        JO messages = GetPrunableMessagesCall.create(IGNIS.getId()).account(ALICE.getRsAccount()).call();
+        JA PrunableMessages = messages.getArray("prunableMessages");
+
+        //  Assert message is send after participation
+        JO Msg = PrunableMessages.get(0);
+        int messageHeight = Msg.getInt("currentHeight");
+        Assert.assertTrue(participationHeight<messageHeight);
+        // TODO Assert 1
+        // TODO Assert 2
+        // assert: correct recipient, message content
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Asserting message content");
+        // TODO Assert 4
+        // assert that no message is sent for incomplete participation
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Asserting that an incomplete participation results in no message to recipient");
+        // TODO Assert 3
+
+        Logger.logDebugMessage("TEST: sendMessageForWinner(): Done");
+    }
+
+    @Test
+    public void dontSendForPreviousRound(){
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void multipleParticipationSplitPot(){
+        Logger.logDebugMessage("TEST: multipleParticipationSplitPot(): Start");
+        JO jackParams = new JO();
+        int contractFrequency = 9;
+        int confirmationTime = 1;
+        int collectionSize = 3;
+        jackParams.put("frequency",contractFrequency);
+        jackParams.put("collectionRs",ALICE.getRsAccount());
+        jackParams.put("confirmationTime",confirmationTime);
+        initCollection(collectionSize);
+
+        String jackName = ContractTestHelper.deployContract(Jackpot.class,jackParams,false);
+
+        generateBlock();
+
+        //JA collectionAssets = TarascaTester.getCollectionAssets(CHUCK.getRsAccount());
+        //sendAssets(collectionAssets,3,CHUCK.getSecretPhrase(),BOB.getRsAccount(),"to Bob");
+
+        Logger.logDebugMessage("TEST: multipleParticipationSplitPot(): Prepare accounts");
+        //JA collectionAssets = TarascaTester.getCollectionAssets();
+        //collectionAssets.stream().forEach(a -> Logger.logDebugMessage("TEST: asset from collection: ",a.toString()));
+        JO response = GetAssetsByIssuerCall.create().account(ALICE.getRsAccount()).call();
+        JA collectionAssets = response.getArray("assets").getArray(0);
+        sendAssets(collectionAssets,3,ALICE.getSecretPhrase(),BOB.getRsAccount(),"to Bob");
+        sendAssets(collectionAssets,3,ALICE.getSecretPhrase(),DAVE.getRsAccount(),"to Bob");
+
+        Logger.logDebugMessage("TEST: Accounts");
+        Logger.logDebugMessage("TEST: Contract (Alice): "+ALICE.getRsAccount()+", numeric: "+ALICE.getAccount());
+        Logger.logDebugMessage("TEST: Player1  (Bob  ): "+BOB.getRsAccount()+", numeric: "+BOB.getAccount());
+        Logger.logDebugMessage("TEST: Player2  (Dave ): "+DAVE.getRsAccount()+", numeric: "+DAVE.getAccount());
+
+        generateBlock();
+        Logger.logDebugMessage("TEST: multipleParticipationSplitPot(): Start playing");
+
+        JO responseFull = GetBalanceCall.create(IGNIS.getId()).account(ALICE.getRsAccount()).call();
+        long balanceFull = Long.parseLong((String) responseFull.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        JO responseBobBefore = GetBalanceCall.create(IGNIS.getId()).account(BOB.getRsAccount()).call();
+        long balanceBobBefore = Long.parseLong((String) responseBobBefore.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        JO responseDaveBefore = GetBalanceCall.create(IGNIS.getId()).account(DAVE.getRsAccount()).call();
+        long balanceDaveBefore = Long.parseLong((String) responseDaveBefore.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        //send not all assets to contract, expectation is jackpot will reject BOB!
+        sendAssets(collectionAssets,1,BOB.getSecretPhrase(),ALICE.getRsAccount(),"from Bob to Contract ALICE");
+        sendAssets(collectionAssets,1,DAVE.getSecretPhrase(),ALICE.getRsAccount(),"from Dave to Contract ALICE");
+
+        generateBlock();
+        sendAssets(collectionAssets,1,BOB.getSecretPhrase(),ALICE.getRsAccount(),"from Bob to Contract ALICE");
+        generateBlock();
+        generateBlock();
+        generateBlock();
+        generateBlock();
+
+        Logger.logDebugMessage("TEST: multipleParticipationSplitPot(): Evaluate results");
+
+
+        JO responseEmpty = GetBalanceCall.create(IGNIS.getId()).account(ALICE.getRsAccount()).call();
+        long balanceEmpty = Long.parseLong((String) responseEmpty.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        JO responseBobAfter = GetBalanceCall.create(IGNIS.getId()).account(BOB.getRsAccount()).call();
+        long balanceBobAfter = Long.parseLong((String) responseBobAfter.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        JO responseDaveAfter = GetBalanceCall.create(IGNIS.getId()).account(DAVE.getRsAccount()).call();
+        long balanceDaveAfter = Long.parseLong((String) responseDaveAfter.get("balanceNQT"))/IGNIS.ONE_COIN;
+
+        long diffBob = balanceBobAfter-balanceBobBefore;
+        long diffDave = balanceDaveAfter-balanceDaveBefore;
+        long diffAlice = balanceEmpty-balanceFull;
+
+        Assert.assertTrue( balanceEmpty<10); // 10 ignis tolerance for fees..
+        Assert.assertTrue( abs(diffBob + (diffAlice+600)*2/3)<10); // 10 ignis tolerance for fees..
+        Assert.assertTrue( abs(diffDave+ (diffAlice+600)*1/3)<10);
+
+        //Assert.assertTrue( abs(diffBob + diffAlice + 300)<10); // 10 ignis tolerance for fees.., 300 Ignis fees with Ardor V2.3.3
+        Logger.logDebugMessage("TEST: multipleParticipationSplitPot(): Done");
+    }
 }
